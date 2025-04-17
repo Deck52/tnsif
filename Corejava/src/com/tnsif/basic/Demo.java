b51b1c5e7dd01e6a35d123d6ddb9d4a0c8a3bbf5
Keshava Narayana <Keshava Narayana@DESKTOP-B54MHV4>
2025-04-17 12:44:58 +0530

    commit 2

--- Corejava/src/com/tnsif/basic/Demo.java
+++ Corejava/src/com/tnsif/basic/Demo.java
@@ -1,8 +1,20 @@
 package com.tnsif.basic;
 
 public class Demo {
+	int USN;
+	String name;
+	int age;
 	
+	public void show() {
+		System.out.println("USN:"+USN+"\nName:"+name+"\nAge:"+age);
+	}
 public static void main(String[] args) {
-	System.out.println("welcome to java");
+	Demo e=new Demo();
+	e.USN=20;
+	e.name="Bharath V";
+	e.age=20;
+	
+	e.show();
+	
 }
 }
