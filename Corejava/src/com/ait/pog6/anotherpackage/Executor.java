be70570ceb839637b1290b0ae4e04cd01e033828
Keshava Narayana <Keshava Narayana@DESKTOP-B54MHV4>
2025-05-14 12:57:51 +0530

    day 4-10

---
+++ Corejava/src/com/ait/pog6/anotherpackage/Executor.java
@@ -0,0 +1,16 @@
+package com.ait.pog6.anotherpackage;
+
+import com.ait.pog6.Base;
+
+public class Executor {
+
+	public static void main(String[] args) {
+		Base b1=new Base();
+		b1.varPrivate=89;
+		b1.varDefault=80;
+		b1.varProtected=89;
+		b1.varPublic=90;
+
+	}
+
+}
---
+++ Corejava/src/com/ait/pog6/polymorphism/OverloadingMethod.java
@@ -0,0 +1,12 @@
+package com.ait.pog6.polymorphism;
+
+public class OverloadingMethod {
+
+	public static void main(String[] args) {
+		System.out.println(MethodOverLoading.addition(5, 2));
+		System.out.println(MethodOverLoading.addition(5,2,1));
+		System.out.println(MethodOverLoading.addition(5,8.4f));
+
+	}
+
+}
