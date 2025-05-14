8f5477c20a3671c1ba4356dead565c95b929139f
Deck52 <bharath.v.2405@gmail.com>
2025-05-14 14:28:42 +0530

    Executor updated

--- Corejava/src/com/ait/pog6/anotherpackage/Executor.java
+++ Corejava/src/com/ait/pog6/anotherpackage/Executor.java
@@ -2,14 +2,17 @@ package com.ait.pog6.anotherpackage;
 
 import com.ait.pog6.Base;
 
-public class Executor {
+public class Executor extends Base {
 
 	public static void main(String[] args) {
-		Base b1=new Base();
-		b1.varPrivate=89;
-		b1.varDefault=80;
+		Executor b1=new Executor();
+//		b1.varPrivate=89;
+//		b1.varDefault=80;
 		b1.varProtected=89;
 		b1.varPublic=90;
+		b1.methodProtected();
+		b1.methodPublic();
+	
 
 	}
 
