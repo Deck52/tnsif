77e72fc532c6023480ff0ebacf6440314380d1dd
Deck52 <bharath.v.2405@gmail.com>
2025-05-23 11:32:02 +0530

    Final prog Update

--- Corejava/src/com/ait/day8/Static/MyClassDemo.java
+++ Corejava/src/com/ait/day8/Static/MyClassDemo.java
@@ -5,6 +5,7 @@ public class MyClassDemo {
 	public static void main(String[] args) {
 		
 			MyClass o1 = new MyClass();
+			System.out.println(o1);
 			MyClass.display();
 			MyClass o2 = new MyClass();
 			System.out.println(o2);
